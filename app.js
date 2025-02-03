260712bb6e4ba32c3ab8dd1c7fe2c836c6e8e683
Hemanth Chebiyam <hc3746@rit.edu>
2025-02-03 10:12:31 -0500

    full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hemanth Chebiyam trying an AWS Elastic Beanstalk Node.js Sample App!'));
+app.get('/', (req, res) => res.send('Hemanth Chebiyam trying an AWS Elastic Beanstalk Node.js Sample App! I am running a code pipeline here.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
